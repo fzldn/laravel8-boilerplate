7e66fc97715630c5908f06af3c5424f6dc5a0aa8
Faizal Nugraha <faizaldwinugraha@gmail.com>
2022-06-07 01:17:42 +0700

    init laravel breeze

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/app.js', 'public/js');
+mix.postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.disableNotifications();
