462dfbe319cbd980f0e1fc55f1b6514dbc807109
Faizal Nugraha <faizaldwinugraha@gmail.com>
2022-06-23 18:26:39 +0700

    layout with title

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -2,10 +2,37 @@
 
 namespace App\View\Components;
 
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class GuestLayout extends Component
 {
+    /**
+     * The page title.
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * Create the component instance.
+     *
+     * @param  string  $type
+     * @param  string  $message
+     * @return void
+     */
+    public function __construct($title = '')
+    {
+        $pageTitle = Str::of(config('app.name'))
+            ->when($title !== '', function ($str) use ($title) {
+                return $str
+                    ->prepend(' - ')
+                    ->prepend($title);
+            });
+
+        $this->title = $pageTitle;
+    }
+
     /**
      * Get the view / contents that represents the component.
      *
